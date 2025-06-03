50b8250488a11a2e85125eb0ec66cc05e936a35d
Birendra275 <bibekbasnet@medhavhi.com>
2025-06-03 16:25:12 +0545

    feat day2 of DSA with java

---
+++ day2/Functions.java
@@ -0,0 +1,50 @@
+import java.util.Scanner; 
+public class Functions{
+ 
+ public static void printName(String name){
+     System.out.println("Your name is " + name);
+     return;
+ }
+
+ public static int sumTwoNumber(int num1,int num2){
+     int sum=num1+num2;
+     System.out.println("The sum of two number is"+sum);
+     return sum;
+ }
+
+
+public static void calculateFactorial(int number){
+    int factorial=1;
+    for(int i=number;i>=1;i--){
+        factorial=factorial*i;
+    }
+    System.out.println("Factorial is "+factorial);
+}
+
+public static void main(String[] args){
+Scanner sc=new Scanner (System.in);
+ System.out.print("\nEnter your name: "); 
+ String name=sc.nextLine();
+ printName(name);
+
+ System.out.print("\nEnter first Number: "); 
+ int num1=sc.nextInt();
+ System.out.print("\nEnter second Number: "); 
+ int num2=sc.nextInt();
+
+ sumTwoNumber(num1,num2);
+System.out.print("\nEnter  Number: "); 
+
+int factorial=sc.nextInt();
+calculateFactorial(factorial);
+
+ sc.close(); 
+
+    }
+
+}
+
+
+
+Funtion to add two numbers
+
